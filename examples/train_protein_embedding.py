6301d428d8f2416e3100d887ff75257ca747019d
AlexTaguchi <thatflippingfool@gmail.com>
2020-03-24 21:28:45 -0700

    BERT

--- examples/train_protein_embedding.py
+++ examples/train_protein_embedding.py
@@ -22,19 +22,29 @@ import torch.optim as optim
 # Parameters
 batch_size = 64
 mask_ratio = 0.15
-model = 'BiLSTM'
+max_length = 512
+# model = 'BiLSTM'
 # model = 'TransformerEncoder'
+model = 'BERT'
 
 # Model settings
 settings = {
-    'BiLSTM':{
+    'BiLSTM': {
         'vocabulary': 25,
         'embedding_size': 100,
         'lstm_size': 64,
         'lstm_layers': 3,
         'dropout': 0.1
     },
-    'TransformerEncoder':{
+    'TransformerEncoder': {
+        'vocabulary': 25,
+        'embedding_size': 200,
+        'transformer_heads': 2,
+        'transformer_size': 200,
+        'transformer_layers': 2,
+        'dropout': 0.1
+    },
+    'BERT': {
         'vocabulary': 25,
         'embedding_size': 200,
         'transformer_heads': 2,
@@ -44,69 +54,6 @@ settings = {
     }
 }
 
-
-class TransformerEncoder(nn.Module):
-    """Transformer encoder based on https://arxiv.org/pdf/1706.03762.pdf"""
-    def __init__(self, vocabulary, embedding_size, transformer_heads,
-                 transformer_size, transformer_layers, dropout=0.0):
-        """Initialize transformer encoder
-        
-        Arguments:
-            vocabulary {int} -- Size of the amino acid vocabulary
-            embedding_size {int} -- Size of amino acid embedding
-            transformer_heads {int} -- Number of transformer encoder attention heads
-            transformer_size {int} -- Size of transformer encoder feedforward network
-            transformer_layers {int} -- Number of transformer encoder layers
-        
-        Keyword Arguments:
-            dropout {float} -- Dropout rate (default: {0.0})
-        """
-        super(TransformerEncoder, self).__init__()
-        self.embedding_size = embedding_size
-
-        # Amino acid embedder and positional encoder
-        self.amino_embedder = nn.Embedding(vocabulary, embedding_size, padding_idx=0)
-        self.positional_encoder = self.encode_positions()
-
-        # Transformer encoder
-        transformer_layer = nn.TransformerEncoderLayer(embedding_size, transformer_heads,
-                                                       transformer_size, dropout)
-        self.transformer_encoder = nn.TransformerEncoder(transformer_layer, transformer_layers)
-        
-        # Linear decoder
-        self.decoder = nn.Linear(embedding_size, vocabulary)
-    
-    def encode_positions(self, max_length=5000):
-        # Preallocate positional encoder
-        positional_encoder = torch.zeros(max_length, self.embedding_size)
-
-        # Calculate frequencies
-        frequencies = torch.arange(0, self.embedding_size, 2).float()
-        frequencies = torch.exp((-math.log(10000.0) / self.embedding_size) * frequencies)
-
-        # Compute sines and cosines of positional encoder
-        positions = torch.arange(0, max_length, dtype=torch.float).unsqueeze(1)
-        positional_encoder[:, 0::2] = torch.sin(positions * frequencies)
-        positional_encoder[:, 1::2] = torch.cos(positions * frequencies)
-        positional_encoder = positional_encoder.unsqueeze(0)
-
-        return positional_encoder
-    
-    def forward(self, sequences):
-
-        # Embed sequences with weighting factor
-        embedding = self.amino_embedder(sequences) * math.sqrt(self.embedding_size)
-
-        # Add positional encoding
-        embedding += self.positional_encoder[:, :embedding.size(1), :]
-
-        # Encode with transformer
-        encoding = self.transformer_encoder(embedding.transpose(0, 1)).transpose(0, 1)
-
-        # Decode with linear layer
-        decoding = self.decoder(encoding)
-
-        return embedding, encoding, decoding
 
 class BiLSTM(nn.Module):
     """Bidirectional LSTM encoder"""
@@ -159,6 +106,116 @@ class BiLSTM(nn.Module):
         return embedding, encoding, decoding
 
 
+class TransformerEncoder(nn.Module):
+    """Transformer encoder based on https://arxiv.org/pdf/1706.03762.pdf"""
+    def __init__(self, vocabulary, embedding_size, transformer_heads,
+                 transformer_size, transformer_layers, dropout=0.0):
+        """Initialize transformer encoder
+        
+        Arguments:
+            vocabulary {int} -- Size of the amino acid vocabulary
+            embedding_size {int} -- Size of amino acid embedding
+            transformer_heads {int} -- Number of transformer encoder attention heads
+            transformer_size {int} -- Size of transformer encoder feedforward network
+            transformer_layers {int} -- Number of transformer encoder layers
+        
+        Keyword Arguments:
+            dropout {float} -- Dropout rate (default: {0.0})
+        """
+        super(TransformerEncoder, self).__init__()
+        self.embedding_size = embedding_size
+
+        # Amino acid embedder and positional encoder
+        self.amino_embedder = nn.Embedding(vocabulary, embedding_size, padding_idx=0)
+        self.positional_encoder = self.encode_positions()
+
+        # Transformer encoder
+        transformer_layer = nn.TransformerEncoderLayer(embedding_size, transformer_heads,
+                                                       transformer_size, dropout)
+        self.transformer_encoder = nn.TransformerEncoder(transformer_layer, transformer_layers)
+        
+        # Linear decoder
+        self.decoder = nn.Linear(embedding_size, vocabulary)
+    
+    def encode_positions(self, max_length=5000):
+        # Preallocate positional encoder
+        positional_encoder = torch.zeros(max_length, self.embedding_size)
+
+        # Calculate frequencies
+        frequencies = torch.arange(0, self.embedding_size, 2).float()
+        frequencies = torch.exp((-math.log(10000.0) / self.embedding_size) * frequencies)
+
+        # Compute sines and cosines of positional encoder
+        positions = torch.arange(0, max_length, dtype=torch.float).unsqueeze(1)
+        positional_encoder[:, 0::2] = torch.sin(positions * frequencies)
+        positional_encoder[:, 1::2] = torch.cos(positions * frequencies)
+        positional_encoder = positional_encoder.unsqueeze(0)
+
+        return positional_encoder
+    
+    def forward(self, sequences):
+        # Embed sequences with weighting factor
+        embedding = self.amino_embedder(sequences) * math.sqrt(self.embedding_size)
+
+        # Add positional encoding
+        embedding += self.positional_encoder[:, :embedding.size(1), :]
+
+        # Encode with transformer
+        encoding = self.transformer_encoder(embedding.transpose(0, 1)).transpose(0, 1)
+
+        # Decode with linear layer
+        decoding = self.decoder(encoding)
+
+        return embedding, encoding, decoding
+
+
+class BERT(nn.Module):
+    """BERT transformer based on https://arxiv.org/pdf/1810.04805.pdf"""
+    def __init__(self, vocabulary, embedding_size, transformer_heads,
+                 transformer_size, transformer_layers, dropout=0.1):
+        """Initialize BERT
+        
+        Arguments:
+            vocabulary {int} -- Size of the amino acid vocabulary
+            embedding_size {int} -- Size of amino acid embedding
+            transformer_heads {int} -- Number of transformer attention heads
+            transformer_size {int} -- Size of transformer feedforward network
+            transformer_layers {int} -- Number of transformer layers
+        
+        Keyword Arguments:
+            dropout {float} -- Dropout rate (default: {0.0})
+        """
+        super(BERT, self).__init__()
+        self.embedding_size = embedding_size
+
+        # Amino acid embedder
+        self.amino_embedder = nn.Embedding(vocabulary, embedding_size, padding_idx=0)
+        
+        # Transformer encoder
+        encoder_norm = nn.LayerNorm(embedding_size)
+        encoder_layer = nn.TransformerEncoderLayer(embedding_size, transformer_heads,
+                                                   transformer_size, dropout)
+        self.encoder = nn.TransformerEncoder(encoder_layer, transformer_layers, encoder_norm)
+
+        # Transformer decoder
+        decoder_norm = nn.LayerNorm(embedding_size)
+        decoder_layer = nn.TransformerDecoderLayer(embedding_size, transformer_heads,
+                                                   transformer_size, dropout)
+        self.decoder = nn.TransformerDecoder(decoder_layer, transformer_layers, decoder_norm)
+    
+    def forward(self, sequences):
+        # Embed sequences
+        embedding = self.amino_embedder(sequences)
+
+        # Encode with transformer
+        encoding = self.encoder(embedding.transpose(0, 1)).transpose(0, 1)
+
+        # Decode with transformer
+        decoding = self.decoder(embedding.transpose(0, 1), encoding.transpose(0, 1)).transpose(0, 1)
+
+        return embedding, encoding, decoding
+
+
 class SequenceDataset(torch.utils.data.Dataset):
     """Dataset for training and evaluating on protein sequence fasta files"""
     def __init__(self, file_path, mask_ratio=0, max_length=5000):
@@ -206,15 +263,19 @@ def collate_sequences(args):
     """Collate sequences, masks, and labels into padded tensors"""
     return [pad_sequence([arg[i] for arg in args], batch_first=True) for i in range(3)]
 
+
 # Initialize dataset and data loader
-dataset = SequenceDataset('.data/swiss-prot/uniprot_sprot.fasta.gz', mask_ratio=mask_ratio)
+dataset = SequenceDataset('.data/swiss-prot/uniprot_sprot.fasta.gz',
+                          mask_ratio=mask_ratio, max_length=max_length)
 data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, collate_fn=collate_sequences)
 
-# Build model
+# Select model
 if model == 'BiLSTM':
     model = BiLSTM(**settings['BiLSTM'])
 elif model == 'TransformerEncoder':
     model = TransformerEncoder(**settings['TransformerEncoder'])
+elif model == 'BERT':
+    model = BERT(**settings['BERT'])
 else:
     raise ValueError('Model not found!')
 
